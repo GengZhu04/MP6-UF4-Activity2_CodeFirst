34021071309e9e38f65636a5b4f704c9230f3e52
oleksandr <a2208033@institutmontilivi.cat>
2024-02-26 17:23:25 +0100

    Employees y Offices funciona

--- MP6-UF4-Activity2_CodeFirst/Dao/IDAODBManager.cs
+++ MP6-UF4-Activity2_CodeFirst/Dao/IDAODBManager.cs
@@ -11,8 +11,10 @@ namespace MP6_UF4_Activity2_CodeFirst.Dao
         // Constant
         public const string CUSTOMER_FILE_PATH = "./Files/CUSTOMERS.csv";
         public const string EMPLOYEES_FILE_PATH = "./Files/EMPLOYEES.csv";
+        public const string OFFICES_FILE_PATH = "./Files/OFFICES.csv";
       
         public bool ImportCustomers();
         public bool ImportEmployees();
+        public bool ImportOffices();
     }
 }

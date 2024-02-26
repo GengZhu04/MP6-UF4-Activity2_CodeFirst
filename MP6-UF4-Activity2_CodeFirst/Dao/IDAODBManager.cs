dd7e8bfb0bcfd56a437d76582bf471600daf1e0a
GengZhu04 <150048047+GengZhu04@users.noreply.github.com>
2024-02-26 16:54:11 +0100

    Customer no funciona

--- MP6-UF4-Activity2_CodeFirst/Dao/IDAODBManager.cs
+++ MP6-UF4-Activity2_CodeFirst/Dao/IDAODBManager.cs
@@ -8,6 +8,9 @@ namespace MP6_UF4_Activity2_CodeFirst.Dao
 {
     public interface IDAODBManager
     {
+        // Constant
+        public const string CUSTOMER_FILE_PATH = "./Files/CUSTOMERS.csv";
+
         public bool ImportCustomers();
     }
 }

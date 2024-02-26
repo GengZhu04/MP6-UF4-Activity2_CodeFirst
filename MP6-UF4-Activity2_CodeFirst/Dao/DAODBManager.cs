8a3ac58e8fc83a39e5b34a625f76f341590409bc
GengZhu04 <150048047+GengZhu04@users.noreply.github.com>
2024-02-26 16:02:36 +0100

    DAO

--- MP6-UF4-Activity2_CodeFirst/Dao/DAODBManager.cs
+++ MP6-UF4-Activity2_CodeFirst/Dao/DAODBManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MP6_UF4_Activity2_CodeFirst.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,15 @@ namespace MP6_UF4_Activity2_CodeFirst.Dao
 {
     public class DAODBManager : IDAODBManager
     {
-        public DAODBManager() 
+        private CompanyDBContext companyDBContext;
+        public DAODBManager(CompanyDBContext companyDBContext) 
         {
-            
+            this.companyDBContext = companyDBContext;
+        }
+
+        public bool ImportCustomers()
+        {
+            throw new NotImplementedException();
         }
     }
 }
